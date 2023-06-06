ea10da9c623d9bc2f4fdaee6250d2396e546eaf9
TongChen <89793506+CTDancer@users.noreply.github.com>
2023-06-06 14:28:12 +0800

    Update loss.py

--- assignment2/fdunn/modules/loss.py
+++ assignment2/fdunn/modules/loss.py
@@ -8,6 +8,8 @@ import sys
 sys.path.append(os.getcwd())
 
 import numpy as np
+import torch
+import torch.nn.functional as F
 from .base import Loss
 
 class BCELoss(Loss):
@@ -29,8 +31,13 @@ class BCELoss(Loss):
         ###########################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
+        # print('input: ', input)
+        # print('target: ', target)
+        input = 1 / (1+np.exp(-input))
         epsilon = 1e-7
         loss = -np.mean(target * np.log(input + epsilon) + (1 - target) * np.log(1 - input + epsilon))
+
+        # print('loss: ', loss)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return loss
@@ -50,102 +57,6 @@ class BCELoss(Loss):
 
 
 class CrossEntropyLoss(Loss):
-    r"""This criterion computes the cross entropy loss between input logits
-    and target.
-
-    It is useful when training a classification problem with `C` classes.
-    If provided, the optional argument :attr:`weight` should be a 1D `Tensor`
-    assigning weight to each of the classes.
-    This is particularly useful when you have an unbalanced training set.
-
-    The `input` is expected to contain the unnormalized logits for each class (which do `not` need
-    to be positive or sum to 1, in general).
-    `input` has to be a Tensor of size :math:`(C)` for unbatched input,
-    :math:`(minibatch, C)` or :math:`(minibatch, C, d_1, d_2, ..., d_K)` with :math:`K \geq 1` for the
-    `K`-dimensional case. The last being useful for higher dimension inputs, such
-    as computing cross entropy loss per-pixel for 2D images.
-
-    The `target` that this criterion expects should contain either:
-
-    - Class indices in the range :math:`[0, C)` where :math:`C` is the number of classes; if
-      `ignore_index` is specified, this loss also accepts this class index (this index
-      may not necessarily be in the class range). The unreduced (i.e. with :attr:`reduction`
-      set to ``'none'``) loss for this case can be described as:
-
-      .. math::
-          \ell(x, y) = L = \{l_1,\dots,l_N\}^\top, \quad
-          l_n = - w_{y_n} \log \frac{\exp(x_{n,y_n})}{\sum_{c=1}^C \exp(x_{n,c})}
-          \cdot \mathbb{1}\{y_n \not= \text{ignore\_index}\}
-
-      where :math:`x` is the input, :math:`y` is the target, :math:`w` is the weight,
-      :math:`C` is the number of classes, and :math:`N` spans the minibatch dimension as well as
-      :math:`d_1, ..., d_k` for the `K`-dimensional case. If
-      :attr:`reduction` is not ``'none'`` (default ``'mean'``), then
-
-      .. math::
-          \ell(x, y) = \begin{cases}
-              \sum_{n=1}^N \frac{1}{\sum_{n=1}^N w_{y_n} \cdot \mathbb{1}\{y_n \not= \text{ignore\_index}\}} l_n, &
-               \text{if reduction} = \text{`mean';}\\
-                \sum_{n=1}^N l_n,  &
-                \text{if reduction} = \text{`sum'.}
-            \end{cases}
-
-      Note that this case is equivalent to applying :class:`~torch.nn.LogSoftmax`
-      on an input, followed by :class:`~torch.nn.NLLLoss`.
-
-    - Probabilities for each class; useful when labels beyond a single class per minibatch item
-      are required, such as for blended labels, label smoothing, etc. The unreduced (i.e. with
-      :attr:`reduction` set to ``'none'``) loss for this case can be described as:
-
-      .. math::
-          \ell(x, y) = L = \{l_1,\dots,l_N\}^\top, \quad
-          l_n = - \sum_{c=1}^C w_c \log \frac{\exp(x_{n,c})}{\sum_{i=1}^C \exp(x_{n,i})} y_{n,c}
-
-      where :math:`x` is the input, :math:`y` is the target, :math:`w` is the weight,
-      :math:`C` is the number of classes, and :math:`N` spans the minibatch dimension as well as
-      :math:`d_1, ..., d_k` for the `K`-dimensional case. If
-      :attr:`reduction` is not ``'none'`` (default ``'mean'``), then
-
-      .. math::
-          \ell(x, y) = \begin{cases}
-              \frac{\sum_{n=1}^N l_n}{N}, &
-               \text{if reduction} = \text{`mean';}\\
-                \sum_{n=1}^N l_n,  &
-                \text{if reduction} = \text{`sum'.}
-            \end{cases}
-
-    .. note::
-        The performance of this criterion is generally better when `target` contains class
-        indices, as this allows for optimized computation. Consider providing `target` as
-        class probabilities only when a single class label per minibatch item is too restrictive.
-
-    Args:
-        reduction (str, optional): Specifies the reduction to apply to the output:
-            ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will
-            be applied, ``'mean'``: the weighted mean of the output is taken,
-            ``'sum'``: the output will be summed. Note: :attr:`size_average`
-            and :attr:`reduce` are in the process of being deprecated, and in
-            the meantime, specifying either of those two args will override
-            :attr:`reduction`. Default: ``'mean'``
-
-    Shape:
-        - Input: Shape :math:`(C)`, :math:`(N, C)` or :math:`(N, C, d_1, d_2, ..., d_K)` with :math:`K \geq 1`
-          in the case of `K`-dimensional loss.
-        - Target: If containing class indices, shape :math:`()`, :math:`(N)` or :math:`(N, d_1, d_2, ..., d_K)` with
-          :math:`K \geq 1` in the case of K-dimensional loss where each value should be between :math:`[0, C)`.
-          If containing class probabilities, same shape as the input and each value should be between :math:`[0, 1]`.
-        - Output: If reduction is 'none', shape :math:`()`, :math:`(N)` or :math:`(N, d_1, d_2, ..., d_K)` with :math:`K \geq 1`
-          in the case of K-dimensional loss, depending on the shape of the input. Otherwise, scalar.
-
-
-        where:
-
-        .. math::
-            \begin{aligned}
-                C ={} & \text{number of classes} \\
-                N ={} & \text{batch size} \\
-            \end{aligned}
-    """
 
     def __init__(self, model, reduction = 'mean') -> None:
         self.input = None
@@ -164,7 +75,13 @@ class CrossEntropyLoss(Loss):
         ###########################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        loss = -np.sum(target * np.log(input))
+        input = torch.from_numpy(input)
+        input = F.softmax(input, dim=1)
+        target = target.unsqueeze(1).expand_as(input)
+        loss = -torch.sum(target * torch.log(input))
+
+        # print('loss: ', loss)
+        # print('target: ', target)
 
         if self.reduction == 'mean':
             loss /= input.size(0)
@@ -179,7 +96,7 @@ class CrossEntropyLoss(Loss):
         # You may use the self.model                                              #
         ###########################################################################
 
-        input_grad = -self.target / self.input
+        input_grad = -self.target.unsqueeze(1) / self.input
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         self.model.backward(input_grad)
